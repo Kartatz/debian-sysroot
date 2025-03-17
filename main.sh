466052c16ec319e0e6f1612042fa3412a549b561
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-03-17 16:15:49 -0300

    Update script

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ declare -r temporary_directory='/tmp/obggcc-sysroot'
 cd "${temporary_directory}"
 
 while read item; do
-	declare distribution="$(jq  --raw-output '.distribution' <<< "${item}")"
+	declare distribution="$(jq --raw-output '.distribution' <<< "${item}")"
 	declare distribution_version="$(jq '.distribution_version' <<< "${item}")"
 	declare glibc_version="$(jq '.glibc_version' <<< "${item}")"
 	declare linux_version="$(jq '.linux_version' <<< "${item}")"
@@ -19,6 +19,10 @@ while read item; do
 	declare packages="$(jq '.packages' <<< "${item}")"
 	declare output_format="$(jq --raw-output '.output_format' <<< "${item}")"
 	declare loader="$(jq --raw-output '.loader' <<< "${item}")"
+	
+	if [ "${distribution_version}" = '2.2' ]; then
+		distribution_version='2'
+	fi
 	
 	declare sysroot_directory="${workdir}/${triplet}${glibc_version}"
 	declare tarball_filename="${sysroot_directory}.tar.xz"
@@ -82,6 +86,8 @@ while read item; do
 		mv './lib'*'/'* "${sysroot_directory}/lib"
 	fi
 	
+	[ -d "${sysroot_directory}/lib/${host}/gconv" ] && rm --recursive "${sysroot_directory}/lib/${host}/gconv"
+	
 	if [ "${distribution}" = 'debian' ]; then
 		if (( distribution_version >= 7 )); then
 			mv "./lib/${host}/"* "${sysroot_directory}/lib"
@@ -100,24 +106,30 @@ while read item; do
 	
 	cd "${sysroot_directory}/lib"
 	
+	[ -d './nptl' ] && rm --recursive './nptl'
+	[ -d './tls' ] && rm --recursive './tls'
+	[ -d './audit' ] && rm --recursive './audit'
+	
+	[ -f './pt_chown' ] && unlink './pt_chown'
+	
 	find . -type l | xargs ls -l | grep --perl-regexp '/lib(?:64)?/' | awk '{print "unlink "$9" && ln -s ./$(basename "$11") ./$(basename "$9")"}' | bash
 	
 	if [ "${triplet}" == 'alpha-unknown-linux-gnu' ] || [ "${triplet}" == 'ia64-unknown-linux-gnu' ]; then
-		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( ./libc.so.6.1 ./libc_nonshared.a  AS_NEEDED ( ./${loader} ) )" > './libc.so'
+		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( libc.so.6.1 libc_nonshared.a AS_NEEDED ( ${loader} ) )" > './libc.so'
 	else
-		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( ./libc.so.6 ./libc_nonshared.a  AS_NEEDED ( ./${loader} ) )" > './libc.so'
+		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( libc.so.6 libc_nonshared.a AS_NEEDED ( ${loader} ) )" > './libc.so'
 	fi
 	
-	if (( distribution_version <= 9 )); then
-		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( ./libpthread.so.0 ./libpthread_nonshared.a  )" > './libpthread.so'
+	if (( distribution_version >= 4 )) && (( distribution_version <= 9 )); then
+		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( libpthread.so.0 libpthread_nonshared.a )" > './libpthread.so'
 	fi
 	
 	if (( distribution_version >= 9 )) && (( distribution_version <= 10 )) && [ "${triplet}" == 'x86_64-unknown-linux-gnu' ]; then
-		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( ./libm.so.6  AS_NEEDED ( ./libmvec_nonshared.a ./libmvec.so.1 ) )" > './libm.so'
+		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( libm.so.6 AS_NEEDED ( libmvec_nonshared.a libmvec.so.1 ) )" > './libm.so'
 	fi
 	
 	if (( distribution_version >= 11 )) && [ "${triplet}" == 'x86_64-unknown-linux-gnu' ]; then
-		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( ./libm.so.6  AS_NEEDED ( ./libmvec.so.1 ) )" > './libm.so'
+		echo -e "OUTPUT_FORMAT(${output_format})\nGROUP ( libm.so.6 AS_NEEDED ( libmvec.so.1 ) )" > './libm.so'
 	fi
 	
 	if [[ "${triplet}" == mips*-unknown-linux-gnu ]] || [ "${triplet}" == 'powerpc-unknown-linux-gnu' ] || [ "${triplet}" == 's390-unknown-linux-gnu' ] || [ "${triplet}" == 'sparc-unknown-linux-gnu' ]; then
@@ -130,7 +142,7 @@ while read item; do
 	
 	echo "- Creating tarball at ${tarball_filename}"
 	
-	tar --directory="$(dirname "${sysroot_directory}")" --create --file=- "$(basename "${sysroot_directory}")" | xz  --compress -9 > "${tarball_filename}"
+	tar --directory="$(dirname "${sysroot_directory}")" --create --file=- "$(basename "${sysroot_directory}")" | xz --compress -9 > "${tarball_filename}"
 	sha256sum "${tarball_filename}" | sed "s|$(dirname "${sysroot_directory}")/||" > "${tarball_filename}.sha256"
 	
 	rm --force --recursive "${sysroot_directory}"
